96dc58ee0a125c08af0d404dcf7264d12e78886c
plfiorini <plfiorini@8f1b5804-98cd-0310-9b8b-c217470a4c7c>
2008-11-05 07:05:34 +0000

    Retrieve protocols list looking into system and user protocol add-ons directories.
    git-svn-id: http://svn.osdrawer.net/imkit@776 8f1b5804-98cd-0310-9b8b-c217470a4c7c

--- libim/Helpers.cpp
+++ libim/Helpers.cpp
@@ -13,42 +13,6 @@
 
 log_importance g_verbosity_level = liDebug;
 
-#if 0
-// Use this to add unlimited buffer size to LOG()
-
-
- To allocate a sufficiently large string and print into  it
-       (code correct for both glibc 2.0 and glibc 2.1):
-              #include <stdio.h>
-              #include <stdlib.h>
-              #include <stdarg.h>
-              char *
-              make_message(const char *fmt, ...) {
-                 /* Guess we need no more than 100 bytes. */
-                 int n, size = 100;
-                 char *p;
-                 va_list ap;
-                 if ((p = malloc (size)) == NULL)
-                    return NULL;
-                 while (1) {
-                    /* Try to print in the allocated space. */
-                    va_start(ap, fmt);
-                    n = vsnprintf (p, size, fmt, ap);
-                    va_end(ap);
-                    /* If that worked, return the string. */
-                    if (n > -1 && n < size)
-                       return p;
-                    /* Else try again with more space. */
-                    if (n > -1)    /* glibc 2.1 */
-                       size = n+1; /* precisely what is needed */
-                    else           /* glibc 2.0 */
-                       size *= 2;  /* twice the old size */
-                    if ((p = realloc (p, size)) == NULL)
-                       return NULL;
-                 }
-              }
-#endif
-
 bool _has_checked_for_tty = false;
 
 void
@@ -437,18 +401,19 @@ im_get_file_list( const char * path, const char * msg_field, BMessage * msg )
 }
 
 void
-im_get_protocol_list( BMessage * list )
+im_get_protocol_list(BMessage* list)
 {
 	BPath path;
-	
-	if (find_directory(B_USER_SETTINGS_DIRECTORY,&path,true,NULL) != B_OK)
-		return;
-	
-	path.Append("im_kit/add-ons/protocols");
-	
-	printf("%s\n", path.Path() );
-	
-	im_get_file_list( path.Path(), "protocol", list);
+
+	if (find_directory(B_COMMON_ADDONS_DIRECTORY, &path, true, NULL) == B_OK) {
+		path.Append("im_kit/protocols");
+		im_get_file_list(path.Path(), "protocol", list);
+	}
+
+	if (find_directory(B_USER_ADDONS_DIRECTORY, &path, true, NULL) == B_OK) {
+		path.Append("im_kit/protocols");
+		im_get_file_list(path.Path(), "protocol", list);
+	}
 }
 
 void
