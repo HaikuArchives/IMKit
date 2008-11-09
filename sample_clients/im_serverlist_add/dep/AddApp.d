4d4881cd4a7f9c9de4d65c6a134757a5da520fb9
slaad <slaad@8f1b5804-98cd-0310-9b8b-c217470a4c7c>
2008-11-09 00:46:17 +0000

    im_serverlist_add: Removed some build files
    git-svn-id: http://svn.osdrawer.net/imkit@797 8f1b5804-98cd-0310-9b8b-c217470a4c7c

--- sample_clients/im_serverlist_add/dep/AddApp.d
+++
@@ -1,2 +0,0 @@
-obj.x86/AddApp.o : 
-dep/AddApp.d obj.x86/AddApp.o : AddApp.cpp AddApp.h ../../common/IMKitUtilities.h
--- sample_clients/im_serverlist_add/dep/IMKitUtilities.d
+++
@@ -1,2 +0,0 @@
-obj.x86/IMKitUtilities.o : 
-dep/IMKitUtilities.d obj.x86/IMKitUtilities.o : ../../common/IMKitUtilities.cpp   ../../common/IMKitUtilities.h
